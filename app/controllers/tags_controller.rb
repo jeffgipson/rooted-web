dcb4bb4d97177b287d7b3d61dfbefd199fd53d14
Steven Masse <semasse2000@yahoo.com>
2019-10-31 11:41:07 -0500

    Renamed type to tagtype for all instances to work correctly.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -69,6 +69,6 @@ class TagsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def tag_params
-      params.require(:tag).permit(:name, :slug, :type)
+      params.require(:tag).permit(:name, :slug, :tagtype)
     end
 end
