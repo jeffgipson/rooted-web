dfc0b320531bd2386d131c95b151e3e8a1c539d3
Steven Masse <semasse2000@yahoo.com>
2019-10-31 11:34:44 -0500

    Changed column in tags "type" to "tagtype".

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_30_002307) do
+ActiveRecord::Schema.define(version: 2019_10_31_163022) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -62,7 +62,7 @@ ActiveRecord::Schema.define(version: 2019_10_30_002307) do
   create_table "tags", force: :cascade do |t|
     t.string "name"
     t.string "slug"
-    t.string "type"
+    t.string "tagtype"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
