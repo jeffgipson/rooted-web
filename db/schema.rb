0e9ddb84d18b11a9aeac31dcf8e38a4d06cd699e
Steven Masse <semasse2000@yahoo.com>
2019-11-02 10:52:56 -0500

    Added reference from tag to images_asset.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_02_154240) do
+ActiveRecord::Schema.define(version: 2019_11_02_154729) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -57,6 +57,8 @@ ActiveRecord::Schema.define(version: 2019_11_02_154240) do
     t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "tag_id"
+    t.index ["tag_id"], name: "index_image_assets_on_tag_id"
   end
 
   create_table "tags", force: :cascade do |t|
@@ -80,4 +82,5 @@ ActiveRecord::Schema.define(version: 2019_11_02_154240) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "image_assets", "tags"
 end
