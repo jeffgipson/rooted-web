f2fd98c1cd1d2b4b39fb2ff5c320bf08c654cab3
Steven Masse <semasse2000@yahoo.com>
2019-11-02 10:33:56 -0500

    Scaffold made for tags.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_003654) do
+ActiveRecord::Schema.define(version: 2019_11_02_153211) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,14 @@ ActiveRecord::Schema.define(version: 2019_10_29_003654) do
     t.string "Google_A_ID"
     t.string "Instagram_URL"
     t.string "Twitter_URL"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.string "slug"
+    t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
