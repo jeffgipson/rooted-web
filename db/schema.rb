b467794d6fef4976b2adfc63111647b0fd80720d
Steven Masse <semasse2000@yahoo.com>
2019-11-02 10:25:13 -0500

    Rolled back migratation to redo tag and image to work correctly with type changed.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_31_163022) do
+ActiveRecord::Schema.define(version: 2019_10_29_003654) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -50,23 +50,6 @@ ActiveRecord::Schema.define(version: 2019_10_31_163022) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "image_assets", force: :cascade do |t|
-    t.string "name"
-    t.string "image"
-    t.integer "tags_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["tags_id"], name: "index_image_assets_on_tags_id"
-  end
-
-  create_table "tags", force: :cascade do |t|
-    t.string "name"
-    t.string "slug"
-    t.string "tagtype"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -80,5 +63,4 @@ ActiveRecord::Schema.define(version: 2019_10_31_163022) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "image_assets", "tags", column: "tags_id"
 end
